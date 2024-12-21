73651d105e97cb856ffaea64401e36ed72d0e23c
engiNERDs <enginerds@tccs.org>
2024-12-21 08:07:02 -0800

    Forgot to git add the rightAuto2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/rightAutonomous2.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.myConstants;
+import org.firstinspires.ftc.teamcode.myLinearOpMode;
+
+@Autonomous
+public class rightAutonomous2 extends myLinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        servoClawLeft.setPosition(myConstants.servoPositions.CLAW_LEFT_CLOSED);
+        servoClawRight.setPosition(myConstants.servoPositions.CLAW_RIGHT_CLOSED);
+        servoArm.setPosition(myConstants.servoPositions.ARM_UP);
+        odo.resetPosAndIMU();
+        waitForStart();
+        setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDrivePower(0.5);
+        Right(2800);
+        servoClawLeft.setPosition(myConstants.servoPositions.CLAW_LEFT_OPEN);
+        servoClawRight.setPosition(myConstants.servoPositions.CLAW_RIGHT_OPEN);
+        servoArm.setPosition(myConstants.servoPositions.ARM_DOWN);
+        sleep(200);
+    }
+}
