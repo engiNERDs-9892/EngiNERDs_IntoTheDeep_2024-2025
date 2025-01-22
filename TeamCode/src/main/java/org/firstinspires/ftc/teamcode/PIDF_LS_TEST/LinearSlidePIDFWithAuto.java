2e29ef8a442019e064680a227cfae4a8f8cde1d6
engiNERDs <enginerds@tccs.org>
2025-01-21 16:09:05 -0800

    made adjustment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_LS_TEST/LinearSlidePIDFWithAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_LS_TEST/LinearSlidePIDFWithAuto.java
@@ -11,6 +11,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -27,6 +28,7 @@ import org.firstinspires.ftc.teamcode.myLinearOpMode;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous(group = "advanced", preselectTeleOp = "enginerdsControl2")
+@Disabled
 public class LinearSlidePIDFWithAuto extends myLinearOpMode {
     private PIDController controller;
 
